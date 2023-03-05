44fd3c11d8d99b475f5c7a3b5a6b38b953df0cda
Juan Salazar <ferthelet@gmail.com>
2023-03-05 07:55:19 -0600

    adds hi to the roommate

--- index.js
+++ index.js
@@ -15,3 +15,16 @@ function logShout(string) {
 function logWhisper(string) {
     console.log(whisper(string));
 }
+
+function sayHiToHeadphonedRoommate(string) {
+    //todo
+    let returnString = "";
+    if (string === "Let's have dinner together!") {
+        returnString = "I would love to!";
+    } else if (string === string.toLowerCase()) {
+        returnString = "I can't hear you!";
+    } else {
+        returnString = "YES INDEED!";
+    }
+    return returnString;
+}
